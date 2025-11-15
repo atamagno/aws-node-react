145b3a2323337560333df8372136111e854839c1
Agustin Tamagno <agusto.tamagno@gmail.com>
2025-11-16 09:28:55 +1100

    Added cors, helmet and express-rate-limit libraries to node backend.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,6 @@
 import cors from "cors";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 import express from "express";
 
@@ -8,8 +10,32 @@ import healthRoutes from "./routes/health";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+// const corsOptions = {
+//   origin: [`http://localhost:${port}`], // TODO: Update with actual frontend origin in production
+// };
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  standardHeaders: true, // return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // disable the `X-RateLimit-*` headers
+  message: {
+    statusCode: 429,
+    message: "Too many requests, please try again after 15 minutes.",
+  },
+});
+
+// middleware to parse JSON request bodies
 app.use(express.json());
+
+// security middleware
+app.use(helmet());
+
+// rate limiting
+app.use(limiter);
+
+// middleware to enable CORS with custom block logic
+app.use(cors());
 
 app.use("/", healthRoutes);
 app.use("/", thingRoutes);
