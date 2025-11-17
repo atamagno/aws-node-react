817118dabe3d7550bfa4aacfe820c92b5bf83e5d
Agustin Tamagno <agusto.tamagno@gmail.com>
2025-11-17 12:36:37 +1100

    Fixed build issue with pino-pretty.

--- backend/esbuild.config.ts
+++ backend/esbuild.config.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import { build } from "esbuild";
 import type { BuildOptions } from "esbuild";
+import esbuildPluginPino from "esbuild-plugin-pino";
 
 import samEntryPoints from "./plugins/esbuild/samEntryPoints";
 
@@ -10,7 +11,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 (async () => {
   try {
-    const entryPoints = [...samEntryPoints("cfn/api-lambda.yaml")];
+    const entryPoints = ["src/index.ts", ...samEntryPoints("cfn/api-lambda.yaml")];
     console.log("Building with entry points:", entryPoints);
     const buildOptions: BuildOptions = {
       entryPoints,
@@ -18,10 +19,14 @@ const isProd = process.env.NODE_ENV === "production";
       platform: "node",
       target: "node20",
       outdir: "dist",
-      outbase: "./src",
       minify: isProd,
       sourcemap: !isProd,
       external: [],
+      plugins: [
+        esbuildPluginPino({
+          transports: ["pino-pretty"],
+        }),
+      ],
     };
     await build(buildOptions);
   } catch (error) {
